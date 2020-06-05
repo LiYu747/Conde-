aab5ddc2fa0ab213081bc3af2daac801d311e008
LiYu747 <1041924015@qq.com>
2020-06-05 19:33:26 +0800

    center

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,49 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+
 Vue.use(VueRouter)
 
   const routes = [
+    {
+      path: '/',
+      name: 'Home',
+      component:Home,
+      children:[
+        {
+          path: 'about',
+          name: 'About',
+          component: () => import ('../views/About')
+        }
+      ]
+    },
+ 
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/sign-in',
+    name: 'sign-in',
+    component: () => import ('../views/Sign-in')
+  },
+  
+  
+
+  
+  
+  {
+    path: '/Details-page',
+    name: 'Details-page',
+    component: () => import ('../views/Details-page')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/Shared',
+    name: 'Shared',
+    component: () => import ('../views/Shared'),
+   
+  
+  },
+ 
+  
+
+ 
 ]
 
 const router = new VueRouter({
